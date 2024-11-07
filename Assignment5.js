3ab46e391e84cd732fca75d8a48905b3a78411d2
Msbee502 <b.hoogesteyn@gmail.com>
2024-11-07 15:11:13 +0100

    Update Assignment5.js

--- Assignment5.js
+++ Assignment5.js
@@ -4,7 +4,9 @@ const account = {
     lastName: "Andersson",
     balance: 500000,
     password: "Banana", 
-    getbalance() {
+
+
+    getBalance() {
         return this.balance;
 
         //this function should display the total amount of the account to the user 
@@ -63,6 +65,48 @@ const account = {
 
 };
 
-console.log(account.login("AnnaAndersson", "Banana"));
-console.log(account.exit ());
-console.log(account.deposit(500));
+
+function atm(){
+    let exit = false;
+
+    while (!exit){
+
+    const choice = parseFloat (
+        prompt (
+            "Select a choice: 1.) See balance 2.) Make a deposit 3.) Make a withdrawal 4.) Get account name 5.) Exit"
+        )
+    );
+
+    switch (choice) {
+        case 1: alert(`Your balance is $${account.getBalance()}`);
+        break;
+
+        case 2: 
+        const depositAmount = parseFloat(prompt("Enter amount to deposit:"));
+        alert(account.deposit(depositAmount));
+        break;
+
+
+        case 3: const withdrawalAmount = parseFloat(prompt("Enter an amount to withdraw:"));
+        alert(account.withdrawal(withdrawalAmount));
+        break;
+
+        case 4: alert(`Account name:${account.accountName}`);
+        break;
+
+        case 5: 
+        account.exit ();
+        exit = true;
+        break;
+
+        default: 
+        alert("Invalid, Please try again.");
+        break;
+
+        }
+    }
+}
+
+console.log(atm());
+// console.log(account.exit ());
+// console.log(account.deposit(1000));
